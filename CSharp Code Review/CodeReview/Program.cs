b06d44ef6414e61767a6de315e33334f0ce8c081
Emilyjohnson25 <emilyjaynegirl@gmail.com>
2018-01-11 16:24:00 -0700

    Added data
    need to add a lable still OPPS

--- CSharp Code Review/CodeReview/Program.cs
+++ CSharp Code Review/CodeReview/Program.cs
@@ -10,13 +10,69 @@ namespace CodeReview
     {
         static void Main(string[] args)
         {
-            //use salutation class for an 'walmart' Greeter   
-            Salutation oldGuy = new Salutation("Welcome to Walmart",
-                "Thanks for shopping at Walmart");
-            //Get the old guy to talk.... 
-            Console.WriteLine(oldGuy.Greet());
-            Console.WriteLine(oldGuy.SayFarewell());
-                  
+            var app = new Program();
+            app.run();
         }
+        //{
+        //    //use salutation class for an 'walmart' Greeter   
+        //    //Salutation oldGuy = new Salutation("Welcome to Walmart",
+        //    //    "Thanks for shopping at Walmart");
+        //    ////Get the old guy to talk.... 
+        //    //Console.WriteLine(oldGuy.Greet());
+        //    //Console.WriteLine(oldGuy.SayFarewell());
+
+        //    //Captain Luke bacard greets the staff
+        //    Salutation klingonCommander = new Salutation("nuqneH!", "Qapla'!");
+        //    //How to get Bacard to say those things
+        //    Console.WriteLine(klingonCommander.Greet());
+        //    Console.WriteLine(klingonCommander.SayFarewell());
+        //    Console.ReadLine();
+        //}
+        //Our "Program" is really the heart of our "application"
+        //So, lets treat it like an object
+        //We'll use a property that holds a bunch of salutation objects
+        public List<Salutation> Speakers { get; private set; } 
+        public Program()
+        {
+            //The job of a constructor is to make sure that all the
+            //properties/fields of the object are set to "meaningful values"
+            Speakers = new List<Salutation>();
+            Speakers.Add(new Salutation ("Welcome to Walmart", "Thanks for shopping at Walmart"));
+            Speakers.Add(new Salutation("nuqneH!", "Qapla'!"));
+
+        }
+        public void run()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("you have entered a large building. Two being stand before you.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Who do you want to speak to? Person A or B? ");
+            string userInput = Console.ReadLine().ToUpper();
+            while(userInput != "A" && userInput != "B")
+            {
+                Console.Write("Try again: ");
+                userInput = Console.ReadLine().ToUpper();
+
+            }
+            Console.ResetColor();
+            switch(userInput)
+            {
+                case "A":
+                    Speak(Speakers[0]);
+                    break;
+                case "B":
+                    Speak(Speakers[1]);
+                    break;
+
+            }
+            //Console.ReadLine();
+        }
+        private void Speak(Salutation speaker)
+        {
+            Console.WriteLine(speaker.Greet());
+            Console.WriteLine(speaker.SayFarewell());
+
+        }
+      
     }
 }
--- CSharp Code Review/CodeReview/Salutation.cs
+++ CSharp Code Review/CodeReview/Salutation.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace CodeReview
 {
+    /// <summary>
+    /// The Salutation class Represents somthing that can say hello or goodbye.
+    /// </summary>
     public class Salutation
     {
+        #region Properties and feilds - What the Salutation Objects will "look like"
+        //Feilds
         private string _Greeting;
         private string _Farewell;
+        #endregion
 
+        #region Constructor and Methods - How the Salutation will "behave"
+        /// <summary>
+        /// Instantiates a Salutation object with the appropriate message 
+        /// </summary>
+        /// <param name="greetingMessage">message to use when greeting</param>
+        /// <param name="farewellMessage">message to use when saying goodbye</param>
         public Salutation(string greetingMessage, string farewellMessage)
         {
             _Greeting = greetingMessage;
@@ -25,5 +37,6 @@ namespace CodeReview
         {
             return _Farewell;
         }
+        #endregion
     }
 }
